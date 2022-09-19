df922981568ab3b61fc9d74a600a2966d53be4ff
heokyeongju <beewt@naver.com>
2022-09-20 08:50:59 +0900

    코드 정리

--- src/Event/Poll.js
+++ src/Event/Poll.js
@@ -6,32 +6,33 @@ const onChange = (checkedValues) => {
 };
 
 function Poll() {
-    return (
-        <div id="Container" /* 전체 */
-             style={{
-                 backgroundColor: '#f8f9fa',
-
-             }}>
-
-            <div id="PollList">
-                <h4>Poll list</h4>
-                <div className="box">
-                    <div className="pollCreate">
-                        <p>Create new poll</p>
-                    </div>
-
-
-
-
-                </div>
-            </div>
-
-            <div id="PollResult">
-                <h4>Poll result</h4>
-                <div className="box"></div>
-            </div>
+  return (
+    <div
+      id="Container" /* 전체 */
+      style={{
+        backgroundColor: '#f8f9fa',
+        height: '725px',
+        marginTop: '100px',
+        paddingTop: '30px',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        flexWrap: 'wrap',
+      }}>
+      <div id="PollList">
+        <h4>Poll list</h4>
+        <div className="box">
+          <div className="pollCreate">
+            <p>Create new poll</p>
+          </div>
         </div>
-    );
+      </div>
+
+      <div id="PollResult">
+        <h4>Poll result</h4>
+        <div className="box"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Poll;
--- src/Event/Question.js
+++ src/Event/Question.js
@@ -1,39 +1,33 @@
 import './Question.css';
 import '../Base/ToastEditor.js';
-import { Input, Button } from 'antd';
+import { Button } from 'antd';
 import React from 'react';
-import EditorBox from "../Base/ToastEditor";
-const { TextArea } = Input;
-
+import EditorBox from '../Base/ToastEditor';
 
 function Question() {
   return (
-    <div id="Container" /* 전체 */
+    <div
+      id="Container" /* 전체 */
       style={{
-          backgroundColor: '#f8f9fa',
-          height: '725px',
-          marginTop : '100px',
-          paddingTop: '30px',
-          display: 'flex',
-          justifyContent : 'space-evenly', // 좀 더 가까이 가능?
-          // alignItems: 'center',
-          flexWrap : 'wrap'
-
+        backgroundColor: '#f8f9fa',
+        height: '725px',
+        marginTop: '100px',
+        paddingTop: '30px',
+        display: 'flex',
+        justifyContent: 'space-evenly',
+        flexWrap: 'wrap',
       }}>
-
       <div id="questionForm">
-          <h4>Question form</h4>
-          <div id ="editor">
-              <EditorBox></EditorBox>
-          </div>
-          <Button className="button">
-              질문 작성
-          </Button>
+        <h4>Question form</h4>
+        <div id="editor">
+          <EditorBox></EditorBox>
+        </div>
+        <Button className="button">질문 작성</Button>
       </div>
 
       <div id="questionList">
-          <h4>Question list</h4>
-          <div className="listBox"></div>
+        <h4>Question list</h4>
+        <div className="listBox"></div>
       </div>
     </div>
   );
