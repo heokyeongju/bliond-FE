357e7dc82427dc10277bbc2e6f24b4bced284613
heokyeongju <beewt@naver.com>
2022-09-24 01:28:31 +0900

    local storage를 이용해 jwt 저장

--- src/pages/Oauth2.js
+++ src/pages/Oauth2.js
@@ -1,29 +1,26 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
-import { useCookies } from 'react-cookie';
 
 const Oauth2 = () => {
   const navigate = useNavigate();
-  const [cookies, setCookie] = useCookies(['jwtCookie']);
 
   useEffect(() => {
-    async function cookie() {
-      const token = await new URL(window.location.href).searchParams.get(
-        'accessToken',
-      );
-      const accessTokenExpiration = await new URL(
-        window.location.href,
-      ).searchParams.get('expiresIn');
-      if (token) {
-        setCookie('jwtCookie', token);
-        setCookie('jwtExpireCookie', accessTokenExpiration);
-      }
+    const token = new URL(window.location.href).searchParams.get('accessToken');
+
+    const accessTokenExpiration = new URL(
+      window.location.href,
+    ).searchParams.get('expiresIn');
+
+    if (token) {
+      localStorage.setItem('accessToken', token);
+      localStorage.setItem('accessTokenExpiration', accessTokenExpiration);
     }
+    console.log('token : ' + token);
+    console.log('local : ' + localStorage.getItem('accessToken'));
+
     navigate('/events');
   }, []);
 
-  console.log('jwt : ' + cookies.jwtCookie);
-  console.log('expire : ' + cookies.jwtExpireCookie);
   return (
     <div>
       <h2>oauth2</h2>
