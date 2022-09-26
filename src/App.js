1cecf33957d469b71ee7400de92694f9cd4f688f
heokyeongju <beewt@naver.com>
2022-09-26 13:11:58 +0900

    axios 연결 오류 해결

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
         });
         console.log(jwt);
         setUserF(response.data);
-        console.log(userInfo);
+        console.log(userF);
       } catch {
         console.log('xxx');
       }
