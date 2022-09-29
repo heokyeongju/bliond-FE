ae065d900755f3a8f96ca72c2871146c643ded8c
doxxx <doxxx93@gmail.com>
2022-09-30 03:14:48 +0900

    다크모드 버튼

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import './Header.css';
-import { Layout } from 'antd';
+import { Layout,Switch } from 'antd';
 import { useParams } from 'react-router-dom';
 
 import React from 'react';
@@ -7,16 +7,21 @@ import { useRecoilState } from 'recoil';
 import { UserAtom } from '../recoil/UserAtom';
 const { Header } = Layout;
 
+const onChange = (checked) => {
+  console.log(`switch to ${checked}`);
+};
 function CustomHeader() {
   const [userInfo, setUserInfo] = useRecoilState(UserAtom);
   const { id } = useParams();
-  console.log('param id : ' + id);
 
+  console.log('param id : ' + id);
   console.log(userInfo);
   return (
     <Layout>
       <Header id="header">
-        <div className="home">Home</div>
+        <div className="home">Home
+          <Switch className="switch" defaultChecked onChange={onChange} />
+        </div>
         <div className="logo">event id : {id}</div>
         <div className="user">{userInfo.nickname}</div>
       </Header>
