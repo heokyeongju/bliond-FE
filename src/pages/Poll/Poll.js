ca1439c6e6343af5df10f98d11d1da848fa06c66
doxxx <doxxx93@gmail.com>
2022-09-30 02:44:08 +0900

    poll 리스트

--- src/pages/Poll/Poll.js
+++ src/pages/Poll/Poll.js
@@ -1,21 +1,38 @@
 import { Button, Modal, Input, Space, Radio, Switch } from 'antd';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
 import './Poll.css';
 import EventLayout from '../../components/EventLayout';
+import {useParams} from "react-router-dom";
+import axios from 'axios';
+
 
 const { TextArea } = Input;
 
 
 const Poll = () => {
+  const {id } = useParams();
 
-  const [value, setValue] = useState(1);
-
+  const [polls, setPolls] = useState([]);
+  useEffect(() => {
+    const jwt = localStorage.getItem('accessToken');
+    (async () => {
+      const { data } = await axios({
+        headers: {
+          authorization: `Bearer ${jwt}`,
+        },
+        method : "get",
+        baseURL : 'http://localhost:3000/api/v1/event/',
+        url : `${id}/polls`
+      });
+      setPolls(data.data);
+    })();
+  } , []);
 
   const onChange = (e) => {
     console.log('radio checked', e.target.value);
-    setValue(e.target.value);
+    setPolls(e.target.value);
   };
 
     const onChange1 = (checked) => {
@@ -60,99 +77,120 @@ const Poll = () => {
     };
 
   return (
-    <>
-      <EventLayout />
-      <div
-        id="Container" /* 전체 */
-        style={{
-          // backgroundColor: '#f8f9fa',
-          height: '725px',
-          marginTop: '80px',
-          paddingTop: '30px',
-          display: 'flex',
-          justifyContent: 'center',
-          flexWrap: 'wrap',
-        }}>
-        <div id="PollList">
-          <div className="pollHeader">
-            <Button
-              icon={<PlusOutlined />}
-              className="pollButton"
-              onClick={showModal}>
-              Create
-            </Button>
-          </div>
-          <div id="PollBox"> poll </div>
-        </div>
-
-        <Modal
-          width={800}
-          style={{ top: 180 }}
-          bodyStyle={{ height: 350 }}
-          open={open}
-          title={
-            <TextArea
-              className="titleText"
-              placeholder="Title"
-              autoSize
-              maxLength={50}
-            />
-          }
-          onOk={handleOk}
-          onCancel={handleCancel}
-          footer={[
-            <Button key="back" onClick={handleCancel}>
-              Return
-            </Button>,
-            <Button
-              key="submit"
-              type="primary"
-              loading={loading}
-              onClick={handleOk}>
-              Send
-            </Button>,
-          ]}>
-            {/* input 추가 및 제거 코드 */}
-          <Space direction="vertical">
-              <div className="App" autoComplete="off">
-                  <div className="form-field">
-                      {serviceList.map((singleService, index) => (
-                          <div key={index} className="services">
-                              <div className="first-division">
-                                  <input
-                                      name="service"
-                                      type="text"
-                                      id="service"
-                                      value={singleService.service}
-                                      onChange={(e) => handleServiceChange(e, index)}
-                                      required/>
-                                  {serviceList.length - 1 === index && serviceList.length < 4 && (
-                                      <button
-                                          type="button"
-                                          onClick={handleServiceAdd}
-                                          className="add-btn">
-                                          <span> + </span>
-                                      </button>
-                                  )}
-                              </div>
-                              <div className="second-division">
-                                  {serviceList.length !== 2 && (
-                                      <button
-                                          type="button"
-                                          onClick={() => handleServiceRemove(index)}
-                                          className="remove-btn">
-                                          <span>X</span>
-                                      </button>
-                                  )}
-                              </div>
+      <>
+        <EventLayout/>
+        <div
+            id="Container" /* 전체 */
+            style={{
+              // backgroundColor: '#f8f9fa',
+              height: '725px',
+              marginTop: '80px',
+              paddingTop: '30px',
+              display: 'flex',
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+            }}>
+          <div id="PollList">
+            <div className="pollHeader">
+              <Button
+                  icon={<PlusOutlined/>}
+                  className="pollButton"
+                  onClick={showModal}>
+                Create
+              </Button>
+            </div>
+            <div id="PollBox1">
+              {polls.map((id) => (
+                  <div id="PollBox">
+                    <div id="PollTitle">
+                      <h3>{id.name}</h3>
+                    </div>
+                    <div id="PollContent">
+                      <p>{id.description}</p>
+                    </div>
+                    <div id="PollChoice">
+                      {id.pollChoices.map((id) => (
+                          <div id="PollChoiceBox">
+                            <Radio.Group onChange={onChange} value={polls}>
+                              <Radio value={id.name}>{id.name}</Radio>
+                            </Radio.Group>
                           </div>
                       ))}
+                    </div>
                   </div>
-              </div>
-          </Space>
-        </Modal>
-      </div>
-    </>
+              ))}
+            </div>
+
+            <Modal
+                width={800}
+                style={{top: 180}}
+                bodyStyle={{height: 350}}
+                open={open}
+                title={
+                  <TextArea
+                      className="titleText"
+                      placeholder="Title"
+                      autoSize
+                      maxLength={50}
+                  />
+                }
+                onOk={handleOk}
+                onCancel={handleCancel}
+                footer={[
+                  <Button key="back" onClick={handleCancel}>
+                    Return
+                  </Button>,
+                  <Button
+                      key="submit"
+                      type="primary"
+                      loading={loading}
+                      onClick={handleOk}>
+                    Send
+                  </Button>,
+                ]}>
+              {/* input 추가 및 제거 코드 */}
+              <Space direction="vertical">
+                <div className="App" autoComplete="off">
+                  <div className="form-field">
+                    {serviceList.map((singleService, index) => (
+                        <div key={index} className="services">
+                          <div className="first-division">
+                            <input
+                                name="service"
+                                type="text"
+                                id="service"
+                                value={singleService.service}
+                                onChange={(e) => handleServiceChange(e, index)}
+                                required/>
+                            {serviceList.length - 1 === index
+                                && serviceList.length < 4 && (
+                                    <button
+                                        type="button"
+                                        onClick={handleServiceAdd}
+                                        className="add-btn">
+                                      <span> + </span>
+                                    </button>
+                                )}
+                          </div>
+                          <div className="second-division">
+                            {serviceList.length !== 2 && (
+                                <button
+                                    type="button"
+                                    onClick={() => handleServiceRemove(index)}
+                                    className="remove-btn">
+                                  <span>X</span>
+                                </button>
+                            )}
+                          </div>
+                        </div>
+                    ))}
+                  </div>
+                </div>
+              </Space>
+            </Modal>
+          </div>
+        </div>
+      </>
   );
 };
 
